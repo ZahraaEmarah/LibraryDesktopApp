45bc3969da72d485e07014216e16b04728a65a9d
ZahraaEmarah <zahraa.emara@syngenta.com>
2023-12-08 12:58:20 +0200

    adding DTO class for book class

---
+++ Library.Domain/BookDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Domain
+{
+    public class BookDTO
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public DateTime PublishDate { get; set; }
+        public string BookISBN { get; set; }
+        public decimal BasePrice { get; set; }
+    }
+    
+}
